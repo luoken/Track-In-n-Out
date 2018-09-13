f20e1f7d37078bbcc2afb28e6ec004cc561985b8
Ken Luo <luozhirong@gmail.com>
2018-09-12 17:59:01 -0700

    unable to get in n out yet, they kicked me out

--- scraper.py
+++ scraper.py
@@ -2,19 +2,25 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import re
 import os
 import pandas as pd
+import requests
 
 url = "http://www.in-n-out.com/locations"
+
+# response = requests.get(url, timeout = 5)
+# page_content = BeautifulSoup(response.content, "html.parser")
+# print(page_content)
 
 browser = webdriver.Firefox()
 browser.get(url)
 browser.implicitly_wait(30)
-browser.get(url)
+# browser.get(url)
 
-python_button = browser.find_element_by_id('resultTabs') 
-print(python_button)
+python_button = browser.find_element_by_tag_name("iframe")
+print(python_button('src'))
 # python_button.click()
 
 #  soup = BeautifulSoup(browser.page_source, 'lxml')
