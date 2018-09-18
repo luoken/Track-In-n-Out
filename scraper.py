db7503585b69e219793538c73f71949c2adbb6ef
Ken Luo <luozhirong@gmail.com>
2018-09-18 00:12:20 -0700

    only able to get first 25 from list..

--- scraper.py
+++ scraper.py
@@ -10,30 +10,41 @@ import requests
 
 url = "http://www.in-n-out.com/locations"
 
-# response = requests.get(url, timeout = 5)
-# page_content = BeautifulSoup(response.content, "html.parser")
-# print(page_content)
-
 browser = webdriver.Firefox()
-browser.get(url)
 browser.implicitly_wait(30)
+browser.get(url)
 innoutiframelink = browser.find_element_by_tag_name("iframe").get_attribute('src')
 
 print(innoutiframelink)
 
 innoutDriver = webdriver.Firefox()
-innoutDriver.get(innoutiframelink)
 innoutDriver.implicitly_wait(30)
-
-print(innoutDriver)
+innoutDriver.get(innoutiframelink)
+innoutDriver.implicitly_wait(100)
 
 iframeClicker = innoutDriver.find_element_by_id('tabAll')
 iframeClicker.click()
-print(iframeClicker)
-# python_button.click()
+iframeClicker = innoutDriver.find_element_by_id('tabAll')
+iframeClicker.click()
+
+soup = BeautifulSoup(innoutDriver.page_source, 'lxml')
+
+# print(soup)
+
+
+data=[]
+x = 0
+for store in soup.find_all('div', id='results'):
+    data.append(store)
+
+print("data")
+for d in data:
+    print(d)
+    print('\n')
+
 
 #  soup = BeautifulSoup(browser.page_source, 'lxml')
 
 # print(soup)
-browser.quit()
-innoutDriver.quit()
+# browser.quit()
+# innoutDriver.quit()
